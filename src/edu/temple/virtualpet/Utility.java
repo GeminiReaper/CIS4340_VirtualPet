e037c14d440fabadadcce65d2273cb59fe5ed55a
Matthew Findlater <matthew.findlater@gmail.com>
2014-11-06 16:41:40 -0500

    created utility class, checks network availability

---
+++ src/edu/temple/virtualpet/Utility.java
@@ -0,0 +1,26 @@
+package edu.temple.virtualpet;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class Utility {
+	
+	public boolean isNetworkAvailable(Activity act) {
+		try {
+		     ConnectivityManager cm = (ConnectivityManager) act.getSystemService(Context.CONNECTIVITY_SERVICE);
+		     NetworkInfo ni = cm.getNetworkInfo(0);
+		     if (ni != null && ni.getState() == NetworkInfo.State.CONNECTED)
+		    	 return true;
+		     ni = cm.getNetworkInfo(1);
+		     if (ni != null && ni.getState() == NetworkInfo.State.CONNECTED)
+		    	 return true;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return false;
+	}
+}
