a7cc1db99193a3b2545094be70286a29af1bcb75
Matthew Findlater <matthew.findlater@gmail.com>
2014-11-06 22:38:20 -0500

    create account and login can switch between each
    Some small fixes to layouts.

--- src/edu/temple/virtualpet/CreateAccountActivity.java
+++ src/edu/temple/virtualpet/CreateAccountActivity.java
@@ -1,15 +1,31 @@
 package edu.temple.virtualpet;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateAccountActivity extends Activity {
+	
+	Button btnLoginAsk;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_account);
+		
+		btnLoginAsk = (Button)findViewById(R.id.btnLoginAsk);
+		
+		btnLoginAsk.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 }
--- src/edu/temple/virtualpet/LoginActivity.java
+++ src/edu/temple/virtualpet/LoginActivity.java
@@ -1,15 +1,31 @@
 package edu.temple.virtualpet;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
+	
+	Button btnCreateAccountAsk;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		btnCreateAccountAsk = (Button)findViewById(R.id.btnCreateAccountAsk);
+		
+		btnCreateAccountAsk.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 }
