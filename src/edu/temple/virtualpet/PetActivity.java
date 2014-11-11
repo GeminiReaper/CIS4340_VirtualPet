db92b442ec87385109bf71945eae204986e854a7
Nicole Maalouf <nicole@temple.edu>
2014-11-10 23:51:39 -0500

    created PetActivity

---
+++ src/edu/temple/virtualpet/PetActivity.java
@@ -0,0 +1,22 @@
+package edu.temple.virtualpet;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PetActivity extends Activity {
+	
+	
+    
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_pet);
+ 
+}
+}
