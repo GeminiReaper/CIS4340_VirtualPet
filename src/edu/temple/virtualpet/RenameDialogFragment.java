de99f6c0fb7c779b33f25ec99e79c4708750da02
Nicole Maalouf <nicole@temple.edu>
2014-11-14 23:47:18 -0500

    rename handler for dismissing dialogfragment

--- src/edu/temple/virtualpet/RenameDialogFragment.java
+++ src/edu/temple/virtualpet/RenameDialogFragment.java
@@ -20,6 +20,7 @@ import org.json.JSONObject;
 import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,84 +28,130 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RenameDialogFragment extends DialogFragment {
 
 	EditText txtNickname;
 	Button btnUpdate;
+
+	private Handler toastHandler = new Handler(new Handler.Callback() {
+		@Override
+		public boolean handleMessage(Message message) {
+			Toast toast = Toast.makeText(getActivity(), (String) message.obj,
+					Toast.LENGTH_LONG);
+			toast.show();
+			return false;
+		}
+	});
 	
-	   static RenameDialogFragment newInstance() {
-	        return new RenameDialogFragment();
-	    }
+	private Handler dismissDialog = new Handler(new Handler.Callback() {
+		@Override
+		public boolean handleMessage(Message message) {
+			getDialog().dismiss();
+			return false;
+		}
+	});
+	
+	
 
+	static RenameDialogFragment newInstance() {
+		return new RenameDialogFragment();
+	}
 
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-            
-        View view = inflater.inflate(R.layout.fragment_rename_dialog, container, false);
-        txtNickname = (EditText) view.findViewById(R.id.txtNickname);
-        getDialog().setTitle("Rename Pet");
-        
-        
-        
-        btnUpdate.setOnClickListener(new View.OnClickListener() {
+		View view = inflater.inflate(R.layout.fragment_rename_dialog,
+				container, false);
+		txtNickname = (EditText) view.findViewById(R.id.txtNickname);
+		getDialog().setTitle("Rename Pet");
+
+		btnUpdate.setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View view) {
-				//http post to update nickname
-				
+				// http post to update nickname
+
 				Thread thread = new Thread() {
 					@Override
 					public void run() {
-						if (Utility
-								.isNetworkAvailable(getActivity())) {
+						if (Utility.isNetworkAvailable(getActivity())) {
 							// Create a new HttpClient and Post Header
 							HttpClient httpclient = new DefaultHttpClient();
 							HttpPost httppost = new HttpPost(
 									"http://cis-linux2.temple.edu/~tuc28686/virtualpet/rename_pet.php");
 
 							// Add your nickname
-									List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(
-											2);
-									nameValuePairs.add(new BasicNameValuePair(
-											"nickname", txtNickname.getText()
-													.toString()));
-									try {
-										httppost.setEntity(new UrlEncodedFormEntity(
-												nameValuePairs));
+							List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(
+									2);
+							nameValuePairs.add(new BasicNameValuePair(
+									"nickname", txtNickname.getText().toString()));
+							nameValuePairs.add(new BasicNameValuePair(
+									"userPetId", UserData.getInstance().getUserPetId()));
+							try {
+								httppost.setEntity(new UrlEncodedFormEntity(
+										nameValuePairs));
 
-										// Execute HTTP Post Request
-										HttpResponse response = httpclient
-												.execute(httppost);
-										
-									} catch (UnsupportedEncodingException e) {
-										// TODO Auto-generated catch block
-										e.printStackTrace();
-									} catch (ClientProtocolException e) {
-										// TODO Auto-generated catch block
-										e.printStackTrace();
-									} catch (IOException e) {
-										// TODO Auto-generated catch block
-										e.printStackTrace();
+								// Execute HTTP Post Request
+								HttpResponse response = httpclient
+										.execute(httppost);
+
+								String responseJSON = EntityUtils
+										.toString(response.getEntity());
+								try {
+									JSONObject jObject = new JSONObject(
+											responseJSON);
+									String result = jObject.getString("result");
+									String message = jObject
+											.getString("message");
+
+									if (!(result.equals("success"))) {
+										// toast
+										Message resultMessage = Message
+												.obtain();
+										resultMessage.obj = result + ": "
+												+ message;
+										toastHandler.sendMessage(resultMessage);
 									}
+									else{
+										//call handler for closing
+										Message resultMessage0 = Message.obtain();
+										resultMessage0.obj = "";
+										dismissDialog.sendMessage(resultMessage0);
+									}
+								} catch (JSONException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
 
-						}
-									else {
-							Message message = Message.obtain();
-							message.obj = "No Network Connection";
-							//toastHandler.sendMessage(message);
-						}
-					
+							} catch (UnsupportedEncodingException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} catch (ClientProtocolException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
 
-				}
+						} else {
+							Message message1 = Message.obtain();
+							message1.obj = "No Network Connection";
+							toastHandler.sendMessage(message1);
+						}
+
+					}
 				};
-				
+
 				thread.start();
+			
+				
 			}
-        });
+		});
 		return view;
 
-}
+	}
 }
